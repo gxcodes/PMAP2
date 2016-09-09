188188d6875b5e408c064f23ae5a2e9aeed1ff32
Angga G. Pratama <gxpratama@gmail.com>
2016-09-09 10:40:13 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# test
+QGIS plugin for testing random things related to PyQGIS
+
+**Installation**
+
+* Download the zip file.
+* Extract it in your QGIS plugins folder.
+* Change the name of the extracted folder to "test".
+* Start your QGIS and go to `Plugins->Manage and Install Plugins`.
+* Type test and check the plugin. 
+
+**Usage**
+
+* Open QGIS and you should notice:
+  * A new menu "My tools"
+  * A new toolbar "My tools"
+  * A missing Edit menu 
+  * A missing File toolbar
---
+++ pmaptools.py
@@ -0,0 +1,104 @@
+"""
+/***************************************************************************
+                                   PMAP Tools
+                  Plugin to show PMAP Desktop IMS Tools Menu
+                              -------------------
+        begin                : 2016-06-20
+        copyright            : (C) 2016 by Angga G. Pratama
+        email                : a.pratama@forestcarbon.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import os
+
+# Import the PyQt and QGIS libraries
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4 import uic
+from qgis.core import *
+
+class PMAPTools:
+
+  def __init__(self, iface):
+    # Save reference to the QGIS interface
+    self.iface = iface
+
+  def initGui(self):
+    # Load pmapDock.ui
+    path = os.path.dirname( os.path.abspath( __file__ ) )
+    self.dock = uic.loadUi( os.path.join( path, "pmapDock.ui" ) )
+    self.iface.addDockWidget( Qt.RightDockWidgetArea, self.dock )
+
+    # self.dock.pushButton.clicked.connect( self.showAbout )
+    self.dock.editAttributeButton.clicked.connect( self.callAttrEditor )
+
+    # Create KKP BPN Link
+    self.action_kkp = QAction( QIcon( os.path.dirname(os.path.realpath(__file__)) + 
+      "/kkp.ico" ), "KKP BPN Pusat", self.iface.mainWindow() )
+    QObject.connect( self.action_kkp, SIGNAL("triggered()"), self.visitBPN )
+
+    # Create InaSAFE Link
+    self.action_inasafe = QAction( QIcon( os.path.dirname(os.path.realpath(__file__)) + 
+      "/inasafe.ico" ), "InaSAFE BNPB", self.iface.mainWindow() )
+    QObject.connect( self.action_inasafe, SIGNAL("triggered()"), self.visitInasafe )
+
+    # Create BKPM Link
+    self.action_bkpm = QAction( QIcon( os.path.dirname(os.path.realpath(__file__)) + 
+      "/bkpm.ico" ), "BKPM SPIPISE", self.iface.mainWindow() )
+    QObject.connect( self.action_bkpm, SIGNAL("triggered()"), self.visitBkpm )
+
+    # Create About Dialog
+    self.show_about = QAction( QIcon( os.path.dirname(os.path.realpath(__file__)) + 
+      "/icon_default.png" ), "Tentang PMaP", self.iface.mainWindow() )
+    QObject.connect( self.show_about, SIGNAL("triggered()"), self.showAbout )
+
+    # Remove QGIS Help menu
+    helpMenu = self.iface.helpMenu()
+    helpMenu.menuAction().setVisible( False )
+
+    # Add a custom menu
+    self.menu = QMenu( "&PMaP Tools", self.iface.mainWindow().menuBar() )
+    self.iface.mainWindow().menuBar().addMenu( self.menu )
+    # Add sub-menus
+    self.menu.addAction( self.action_kkp )
+    self.menu.addAction( self.action_inasafe )
+    self.menu.addAction( self.action_bkpm )
+    self.menu.addSeparator()
+    self.menu.addAction( self.show_about )
+    
+    # Add a custom toolbar
+    self.toolbar = self.iface.addToolBar( "PMAP Model" )
+    self.toolbar.addAction( self.show_about )
+
+  def unload(self):
+    # Remove the plugin menu item and icon
+    self.menu.deleteLater()
+    self.toolbar.deleteLater()
+
+  # visit link methods
+  def visitBPN(self):
+    QDesktopServices.openUrl( QUrl("http://kkp.bpn.go.id/") )
+
+  def visitInasafe(self):
+    QDesktopServices.openUrl( QUrl("http://inasafe.org/") )
+
+  def visitBkpm(self):
+    QDesktopServices.openUrl( QUrl("https://online-spipise.bkpm.go.id/") )
+
+  def showAbout(self):
+    QMessageBox.information( self.iface.mainWindow(), 
+      "Tentang PMaP", 
+      "Participatory Mapping and Planning (PMaP) atau Pemetaan dan Perencanaan Partisipatif merupakan bagian dari Proyek Kemakmuran Hijau yang bertujuan untuk merepresentasikan informasi spasial wilayah lokal yang mengkombinasikan teknologi kartografi modern dengan metode-metode partisipatif.", 
+      QMessageBox.Ok )
+
+  def callAttrEditor(self):
+    self.iface.showAttributeTable( self.iface.activeLayer() )
